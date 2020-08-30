1c29cdc0e020eb62d4d57f3f497a0968301d482c
shijianlei <1664004642@qq.com>
2020-08-30 23:09:37 +0800

    feat: 增加zk模块

--- spi-serviceloader-sample/src/main/java/com/sjl/spi/test/ServiceLoaderTest.java
+++ spi-serviceloader-sample/src/main/java/com/sjl/spi/test/ServiceLoaderTest.java
@@ -1,8 +1,11 @@
 package com.sjl.spi.test;
 
+import com.esotericsoftware.reflectasm.MethodAccess;
 import com.sjl.spi.annotation.ServiceName;
 import com.sjl.spi.service.Invoke;
+import com.sjl.spi.service.impl.DubboInvokeImpl;
 
+import java.math.BigDecimal;
 import java.util.Iterator;
 import java.util.ServiceLoader;
 
@@ -12,16 +15,28 @@ import java.util.ServiceLoader;
  * @description:
  */
 public class ServiceLoaderTest {
-    public static void main(String[] args) {
-        System.out.println("======================开始====================");
-        ServiceLoader<Invoke> serviceLoader = ServiceLoader.load(Invoke.class, Thread.currentThread().getContextClassLoader());
-        final Iterator<Invoke> iterator = serviceLoader.iterator();
-        while (iterator.hasNext()) {
-            final boolean annotationPresent = iterator.next().getClass()
-                    .isAnnotationPresent(ServiceName.class);
-            if (annotationPresent) {
-                System.out.println("=====>>" + iterator.next().invoke("664139849448"));
-            }
-        }
+  public static void main(String[] args) throws IllegalAccessException, InstantiationException {
+    System.out.println("======================开始====================");
+    ServiceLoader<Invoke> serviceLoader =
+        ServiceLoader.load(Invoke.class, Thread.currentThread().getContextClassLoader());
+    final Iterator<Invoke> iterator = serviceLoader.iterator();
+    while (iterator.hasNext()) {
+      final boolean annotationPresent =
+          iterator.next().getClass().isAnnotationPresent(ServiceName.class);
+      if (annotationPresent) {
+        System.out.println("=====>>" + iterator.next().invoke("664139849448"));
+      }
     }
+    String min = "200000.12";
+    String max = "1000";
+    BigDecimal minMoney = new BigDecimal(min);
+    BigDecimal maxMoney = new BigDecimal(max);
+    System.out.println(minMoney.compareTo(maxMoney));
+    int minValueBits = new BigDecimal("198000000000000000000.2").precision();
+    System.out.println(minValueBits);
+
+    MethodAccess methodAccess = MethodAccess.get(DubboInvokeImpl.class);
+    Object invoke = methodAccess.invoke(DubboInvokeImpl.class.newInstance(), "invoke", "455666");
+    System.out.println("--->>"+invoke.toString());
+  }
 }
---
+++ zookeeper-curator-framework/src/main/java/com/seemice/curator/listener/CuratorPathChildrenCacheDemo.java
@@ -0,0 +1,33 @@
+package com.seemice.curator.listener;
+
+import com.seemice.curator.conntect.ZkConnect;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache;
+import org.apache.zookeeper.CreateMode;
+
+/**
+ * @author: JianLei
+ * @date: 2020/8/30 1:18 下午
+ * @description: <p>PathChildrenCache用于监听数据节点子节点的变化情况。当前版本总共提供了7个构造方法，
+ * PathChildrenCache不会对二级子节点进行监听，只会对子节点进行监听。
+ * 其中2个已经不建议使用了</p>
+ */
+
+public class CuratorPathChildrenCacheDemo {
+  public static void main(String[] args) throws Exception {
+      CuratorFramework client = ZkConnect.getInstance();
+      String parentPath = "/p1";
+
+      PathChildrenCache pathChildrenCache = new PathChildrenCache(client,parentPath,true);
+      pathChildrenCache.start(PathChildrenCache.StartMode.POST_INITIALIZED_EVENT);
+      pathChildrenCache.getListenable().addListener((client1, event) -> System.out.println("事件类型："  + event.getType() + "；操作节点：" + event.getData().getPath()));
+
+      String path = "/p1/c1";
+      client.create().withMode(CreateMode.PERSISTENT).forPath(path);
+      Thread.sleep(1000); // 此处需留意，如果没有线程睡眠则无法触发监听事件
+      client.delete().forPath(path);
+
+      Thread.sleep(15000);
+
+  }
+  }
